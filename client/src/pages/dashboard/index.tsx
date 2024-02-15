d97ac034114eb3f965f5148b451134157d4ceaa5
Vineeth-07 <vineethdvs7@gmail.com>
2024-02-15 09:35:10 +0530

    Auto deployment

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -5,7 +5,6 @@ import Post from "../posts/index";
 const Dashboard: React.FC = () => {
   return (
     <div className="static min-h-screen flex-row justify-center bg-gradient-to-r from-purple-500 to-indigo-500">
-      {/* test */}
       <AccountLayout />
       <Post />
     </div>
