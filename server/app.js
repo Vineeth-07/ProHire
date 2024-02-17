8aee607706a59a3bd3f466dcfcb2a4c2c369d883
Vineeth-07 <vineethdvs7@gmail.com>
2024-02-17 10:22:17 +0530

    testing pipeline

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const postRoutes = require("./routes/post");
 
-//routes
+//routes for post
 app.use("/post", postRoutes);
 
 module.exports = app;
